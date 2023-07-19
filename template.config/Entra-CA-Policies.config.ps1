be0a3af9fea3a949f90540a9a90c0d6633a3f881
Julian Pawlowski <75446+jpawlowski@users.noreply.github.com>
2023-07-19 17:05:57 +0200

    update

--- template.config/Entra-CA-Policies.config.ps1
+++ template.config/Entra-CA-Policies.config.ps1
@@ -1,4 +1,8 @@
 #Requires -Version 7.2
 
-$EntraCAPolicyDisplayNamePrefix = $DisplayNamePrefix
+$EntraCAPolicyDisplayNamePrefix = $CompanyNameShort
+$EntraCAPolicyTier0DisplayNamePrefix = @($EntraCAPolicyDisplayNamePrefix, '0') | Join-String -Separator $DisplayNameElementSeparator
+$EntraCAPolicyTier1DisplayNamePrefix = @($EntraCAPolicyDisplayNamePrefix, '1') | Join-String -Separator $DisplayNameElementSeparator
+$EntraCAPolicyTier2DisplayNamePrefix = @($EntraCAPolicyDisplayNamePrefix, '2') | Join-String -Separator $DisplayNameElementSeparator
+
 $EntraCAPoliciesSubfolder = 'Entra-CA-Policies.config'
