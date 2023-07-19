7a0853ce982f877e0942ed0bb6fc042863ba9f55
Julian Pawlowski <75446+jpawlowski@users.noreply.github.com>
2023-07-19 17:07:57 +0200

    update

--- template.config/Entra-CA-Policies.config.ps1
+++ template.config/Entra-CA-Policies.config.ps1
@@ -1,8 +1,8 @@
 #Requires -Version 7.2
 
 $EntraCAPolicyDisplayNamePrefix = $CompanyNameShort
-$EntraCAPolicyTier0DisplayNamePrefix = @($EntraCAPolicyDisplayNamePrefix, '0') | Join-String -Separator $DisplayNameElementSeparator
-$EntraCAPolicyTier1DisplayNamePrefix = @($EntraCAPolicyDisplayNamePrefix, '1') | Join-String -Separator $DisplayNameElementSeparator
-$EntraCAPolicyTier2DisplayNamePrefix = @($EntraCAPolicyDisplayNamePrefix, '2') | Join-String -Separator $DisplayNameElementSeparator
+$EntraCAPolicyTier0DisplayNamePrefix = @($EntraCAPolicyDisplayNamePrefix, 'T0') | Join-String -Separator $DisplayNameElementSeparator
+$EntraCAPolicyTier1DisplayNamePrefix = @($EntraCAPolicyDisplayNamePrefix, 'T1') | Join-String -Separator $DisplayNameElementSeparator
+$EntraCAPolicyTier2DisplayNamePrefix = @($EntraCAPolicyDisplayNamePrefix, 'T2') | Join-String -Separator $DisplayNameElementSeparator
 
 $EntraCAPoliciesSubfolder = 'Entra-CA-Policies.config'
--- template.config/Entra-Groups.config.ps1
+++ template.config/Entra-Groups.config.ps1
@@ -1,4 +1,8 @@
 #Requires -Version 7.2
 
 $EntraGroupsDisplayNamePrefix = $CompanyNameShort
+$EntraGroupsTier0DisplayNamePrefix = @($EntraGroupsDisplayNamePrefix, 'T0') | Join-String -Separator $DisplayNameElementSeparator
+$EntraGroupsTier1DisplayNamePrefix = @($EntraGroupsDisplayNamePrefix, 'T1') | Join-String -Separator $DisplayNameElementSeparator
+$EntraGroupsTier2DisplayNamePrefix = @($EntraGroupsDisplayNamePrefix, 'T2') | Join-String -Separator $DisplayNameElementSeparator
+
 $EntraGroupsSubfolder = 'Entra-Groups.config'
