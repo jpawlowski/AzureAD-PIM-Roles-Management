06eb3009803e0cceb53e575489dac6c7afaa2786
Julian Pawlowski <75446+jpawlowski@users.noreply.github.com>
2023-11-01 20:21:50 +0100

    add Tier1 IAM script

---
+++ Scripts/Get-Temporary-AccessPass-for-Initial-MFA-Setup.ps1
@@ -0,0 +1,106 @@
+<#
+.SYNOPSIS
+    Start Azure Automation Runbook and retrieve new Temporary Access Pass for initial MFA setup
+
+.DESCRIPTION
+    Start Azure Automation Runbook and retrieve new Temporary Access Pass for initial MFA setup.
+    This is just an example script. You SHOULD save your client secret encrypted instead of in this file,
+    or use certificate authentication to connect to Azure.
+
+    Also make sure to change the defaults of the following parameters for unattended run of this script:
+
+    - TenantId
+    - Subscription
+    - ResourceGroupName
+    - AutomationAccountName
+
+.PARAMETER TenantId
+    Tenant ID
+
+.PARAMETER Subscription
+    Subscription Name
+
+.PARAMETER ResourceGroupName
+    ResourceGroupName
+
+.PARAMETER AutomationAccountName
+    AutomationAccountName
+
+.PARAMETER UserId
+    UserId
+
+.NOTES
+    Filename: Get-Temporary-AccessPass-for-Initial-MFA-Setup.ps1
+    Author: Julian Pawlowski <metres_topaz.0v@icloud.com>
+    Version: 1.0
+#>
+#Requires -Version 5.1
+#Requires -Modules @{ ModuleName='Az.Accounts'; ModuleVersion='2.12' }
+#Requires -Modules @{ ModuleName='Az.Automation'; ModuleVersion='1.9' }
+
+Param (
+    [Parameter(Position = 0)]
+    [string]$TenantId = '8cb0668a-9ecd-4147-b144-fa23663291a8',
+    [Parameter(Position = 1)]
+    [string]$Subscription = '6f0ea36a-a698-47f3-a52d-2aeb2d2d03cc',
+    [Parameter(Position = 2)]
+    [string]$ResourceGroupName = 'corp-iam-automations-rg',
+    [Parameter(Position = 3)]
+    [string]$AutomationAccountName = 'corp-iam-automations',
+    [Parameter(Position = 4, mandatory = $true)]
+    [string]$UserId
+)
+
+$ClientId = '4e85a7f1-28ef-48e3-a55b-e7fea66ca1bb'          # Fill in your client ID
+$ClientSecret = 'ThisIsMySecretInClearText'                 # Fill in your client secret
+
+if ("AzureAutomation/" -eq $env:AZUREPS_HOST_ENVIRONMENT -or $PSPrivateMetadata.JobId) {
+    Throw 'This script must not be run from an Azure Automation runbook.'
+}
+
+if (
+    (-Not (Get-AzContext)) -or
+    ($TenantId -ne (Get-AzContext).Tenant)
+) {
+    $SecureStringPwd = $ClientSecret | ConvertTo-SecureString -AsPlainText -Force
+    $PSCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $ClientId, $SecureStringPwd
+
+    Connect-AzAccount `
+        -ServicePrincipal `
+        -Credential $PSCredential `
+        -TenantId $TenantId `
+        -Subscription $Subscription `
+        -Scope Process
+}
+elseif (
+    ($Subscription -ne (Get-AzContext).Subscription.Name) -and
+    ($Subscription -ne (Get-AzContext).Subscription.Id)
+) {
+    $null = Set-AzContext -Subscription $Subscription -WarningAction SilentlyContinue
+}
+
+$job = Start-AzAutomationRunbook `
+    -ResourceGroupName $ResourceGroupName `
+    -AutomationAccountName $AutomationAccountName `
+    -Name 'New-Temporary-Access-Pass-for-Initial-MFA-Setup' `
+    -Parameters @{'UserId' = $UserId }
+
+$doLoop = $true
+While ($doLoop) {
+    $job = Get-AzAutomationJob `
+        -ResourceGroupName $job.ResourceGroupName `
+        -AutomationAccountName $job.AutomationAccountName `
+        -Id $job.JobId
+    $status = $job.Status
+    $doLoop = (($status -ne 'Completed') -and ($status -ne 'Failed') -and ($status -ne 'Suspended') -and ($status -ne 'Stopped'))
+}
+
+$outputObj = Get-AzAutomationJobOutput `
+    -ResourceGroupName $job.ResourceGroupName `
+    -AutomationAccountName $job.AutomationAccountName `
+    -Id $job.JobId `
+    -Stream Output
+
+$outputJson = ConvertFrom-Json -InputObject $outputObj.Summary
+
+return $outputJson.data.TemporaryAccessPass.TemporaryAccessPass
---
+++ Scripts/New-AzAdServicePrincipal-for-Tier1-IAM.ps1
@@ -0,0 +1,120 @@
+<#
+.SYNOPSIS
+    Create a new Microsoft Entra Service Principal to execute runbooks in our Tier 0 Azure Automation Account
+
+.DESCRIPTION
+    Create a new Microsoft Entra Service Principal to execute runbooks in our Tier 0 Azure Automation Account
+
+.PARAMETER TenantId
+    Tenant ID
+
+.PARAMETER Subscription
+    Subscription Name
+
+.PARAMETER ResourceGroupName
+    ResourceGroupName
+
+.PARAMETER Name
+    Name
+
+.NOTES
+    Filename: New-AzAdServicePrincipal-for-Tier1-IAM.ps1
+    Author: Julian Pawlowski <metres_topaz.0v@icloud.com>
+    Version: 1.0
+#>
+#Requires -Version 5.1
+#Requires -Modules @{ ModuleName='Az.Accounts'; ModuleVersion='2.12' }
+#Requires -Modules @{ ModuleName='Az.Resources'; ModuleVersion='6.8' }
+
+[CmdletBinding(
+    SupportsShouldProcess,
+    ConfirmImpact = 'High'
+)]
+Param (
+    [Parameter(Position = 0, mandatory = $true)]
+    [string]$TenantId,
+    [Parameter(Position = 1, mandatory = $true)]
+    [string]$Subscription,
+    [Parameter(Position = 2, mandatory = $true)]
+    [string]$ResourceGroupName,
+    [Parameter(Position = 3, mandatory = $true)]
+    [string]$AutomationAccountName,
+    [Parameter(Position = 4, mandatory = $true)]
+    [string]$Name
+)
+
+if ("AzureAutomation/" -eq $env:AZUREPS_HOST_ENVIRONMENT -or $PSPrivateMetadata.JobId) {
+    Throw 'This script must be run interactively by a privileged administrator account.'
+}
+
+if (
+    (-Not (Get-AzContext)) -or
+    ($TenantId -ne (Get-AzContext).Tenant)
+) {
+    Connect-AzAccount `
+        -UseDeviceAuthentication `
+        -TenantId $TenantId `
+        -Subscription $Subscription `
+        -Scope Process
+}
+elseif (
+    ($Subscription -ne (Get-AzContext).Subscription.Name) -and
+    ($Subscription -ne (Get-AzContext).Subscription.Id)
+) {
+    Set-AzContext -Subscription $Subscription -WarningAction SilentlyContinue
+}
+
+$automationAccount = Get-AzAutomationAccount `
+    -ResourceGroupName $ResourceGroupName `
+    -Name $AutomationAccountName `
+    -ErrorAction SilentlyContinue
+
+if (-Not $automationAccount) {
+    Throw "Please make sure to create the Azure Automation Account first."
+}
+
+$ServicePrincipal = Get-AzADServicePrincipal `
+    -DisplayName $Name `
+    -ErrorAction SilentlyContinue
+
+if (-Not $ServicePrincipal) {
+    if ($PSCmdlet.ShouldProcess(
+            "Create Microsoft Entra Service Principal $($Name)",
+            "Do you confirm to create new Microsoft Entra Service Principal $($Name) ?",
+            'Create new Microsoft Entra Service Principal'
+        )) {
+        $ServicePrincipal = New-AzADServicePrincipal `
+            -DisplayName $Name
+        Write-Output 'Note: Save these information with care!'
+        Write-Output ('Display Name: ' + $ServicePrincipal.DisplayName)
+        Write-Output ('Application (client) ID: ' + $ServicePrincipal.AppId)
+        Write-Output ('Client Secret: ' + $ServicePrincipal.PasswordCredentials.SecretText)
+    }
+    elseif ($WhatIfPreference) {
+        Write-Verbose 'Simulation Mode: A new Microsoft Entra Service Principal would have been created.'
+    }
+    else {
+        Write-Verbose 'Creation of new Microsoft Entra Service Principal was aborted.'
+        exit
+    }
+}
+
+if ($PSCmdlet.ShouldProcess(
+        "Assign desired Azure roles to Microsoft Entra Service Principal $($ServicePrincipal.AppId) ($($ServicePrincipal.DisplayName)) to access Azure Automation Account $($automationAccount.AutomationAccountName)",
+        "Do you confirm to assign desired Azure roles to Service Principal $($ServicePrincipal.DisplayName) to access Azure Automation Account $($automationAccount.AutomationAccountName)?",
+        'Assign Azure roles to Microsoft Entra Service Principal'
+    )) {
+
+    $Scope = "/subscriptions/$($automationAccount.SubscriptionId)/resourceGroups/$($automationAccount.ResourceGroupName)/providers/Microsoft.Automation/automationAccounts/$($automationAccount.AutomationAccountName)"
+    $AzRoles = @(
+        'Automation Job Operator'
+    )
+
+    foreach ($RoleName in $AzRoles) {
+        Write-Output "   Assigning: $RoleName"
+        $null = New-AzRoleAssignment -ObjectId $ServicePrincipal.Id `
+            -RoleDefinitionName $RoleName `
+            -Scope $Scope `
+            -ErrorAction SilentlyContinue
+    }
+}
