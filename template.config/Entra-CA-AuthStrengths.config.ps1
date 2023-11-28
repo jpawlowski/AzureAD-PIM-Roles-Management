b40d2cfe00fdb19e447af98d60431f4dcf8c6c46
Julian Pawlowski <75446+jpawlowski@users.noreply.github.com>
2023-11-28 10:56:48 +0100

    add AADGUIDs for passkeys

--- template.config/Entra-CA-AuthStrengths.config.ps1
+++ template.config/Entra-CA-AuthStrengths.config.ps1
@@ -45,6 +45,24 @@ $EntraCAAuthStrengths = @(
                         '149a2021-8ef6-4133-96b8-81f8d5b7f1f5' # Security Key by Yubico with NFC (Firmware 5.2, 5.4)
                         'a4e9fc6d-4cbe-4758-b8ba-37598bb5bbaa' # Security Key by Yubico with NFC - Black (Firmware 5.4)
                         '0bb43545-fd2c-4185-87dd-feb0b2916ace' # Security Key by Yubico with NFC - Enterprise Edition (Firmware 5.4)
+
+                        # From https://github.com/passkeydeveloper/passkey-authenticator-aaguids/blob/main/aaguid.json
+                        # 'dd4ec289-e01d-41c9-bb89-70fa845d4bf2' # Apple iCloud Keychain (Managed)
+                        # 'd548826e-79b4-db40-a3d8-11116f7e8349' # Bitwarden
+                        # 'adce0002-35bc-c60a-648b-0b25f1f05503' # Chrome on Mac
+                        # 'b5397666-4885-aa6b-cebf-e52262a439a2' # Chromium Browser
+                        # '531126d6-e717-415c-9320-3d9aa6981239' # Dashlane
+                        # '771b48fd-d3d4-4f74-9232-fc157ab0507a' # Edge on Mac
+                        # 'f3809540-7f14-49c1-a8b3-8f813b225541' # Enpass
+                        # 'ea9b8d66-4d01-1d21-3ce4-b6b48cb575d4' # Google Password Manager
+                        # '39a5647e-1853-446c-a1f6-a79bae9f5bc7' # IDmelon
+                        # '0ea242b4-43c4-4a1b-8b17-dd6d0b6baec6' # Keeper
+                        # 'b84e4048-15dc-4dd0-8640-f4f60813c8af' # NordPass
+                        # '53414d53-554e-4700-0000-000000000000' # Samsung Pass
+                        # '08987058-cadc-4b81-b6e1-30de50dcbe96' # Windows Hello Hardware Authenticator
+                        # '6028b017-b1d4-4c02-b4b3-afcdafc96bb2' # Windows Hello Software Authenticator
+                        # '9ddd1817-af5a-4672-a2b9-3e3dd95000a9' # Windows Hello VBS Hardware Authenticator
+                        # 'bada5566-a7aa-401f-bd96-45619a55120d' # 1Password
                     )
                 }
             }
@@ -81,6 +99,24 @@ $EntraCAAuthStrengths = @(
                         '149a2021-8ef6-4133-96b8-81f8d5b7f1f5' # Security Key by Yubico with NFC (Firmware 5.2, 5.4)
                         'a4e9fc6d-4cbe-4758-b8ba-37598bb5bbaa' # Security Key by Yubico with NFC - Black (Firmware 5.4)
                         '0bb43545-fd2c-4185-87dd-feb0b2916ace' # Security Key by Yubico with NFC - Enterprise Edition (Firmware 5.4)
+
+                        # From https://github.com/passkeydeveloper/passkey-authenticator-aaguids/blob/main/aaguid.json
+                        # 'dd4ec289-e01d-41c9-bb89-70fa845d4bf2' # Apple iCloud Keychain (Managed)
+                        # 'd548826e-79b4-db40-a3d8-11116f7e8349' # Bitwarden
+                        # 'adce0002-35bc-c60a-648b-0b25f1f05503' # Chrome on Mac
+                        # 'b5397666-4885-aa6b-cebf-e52262a439a2' # Chromium Browser
+                        # '531126d6-e717-415c-9320-3d9aa6981239' # Dashlane
+                        # '771b48fd-d3d4-4f74-9232-fc157ab0507a' # Edge on Mac
+                        # 'f3809540-7f14-49c1-a8b3-8f813b225541' # Enpass
+                        # 'ea9b8d66-4d01-1d21-3ce4-b6b48cb575d4' # Google Password Manager
+                        # '39a5647e-1853-446c-a1f6-a79bae9f5bc7' # IDmelon
+                        # '0ea242b4-43c4-4a1b-8b17-dd6d0b6baec6' # Keeper
+                        # 'b84e4048-15dc-4dd0-8640-f4f60813c8af' # NordPass
+                        # '53414d53-554e-4700-0000-000000000000' # Samsung Pass
+                        # '08987058-cadc-4b81-b6e1-30de50dcbe96' # Windows Hello Hardware Authenticator
+                        # '6028b017-b1d4-4c02-b4b3-afcdafc96bb2' # Windows Hello Software Authenticator
+                        # '9ddd1817-af5a-4672-a2b9-3e3dd95000a9' # Windows Hello VBS Hardware Authenticator
+                        # 'bada5566-a7aa-401f-bd96-45619a55120d' # 1Password
                     )
                 }
             }
@@ -117,6 +153,24 @@ $EntraCAAuthStrengths = @(
                         '149a2021-8ef6-4133-96b8-81f8d5b7f1f5' # Security Key by Yubico with NFC (Firmware 5.2, 5.4)
                         'a4e9fc6d-4cbe-4758-b8ba-37598bb5bbaa' # Security Key by Yubico with NFC - Black (Firmware 5.4)
                         '0bb43545-fd2c-4185-87dd-feb0b2916ace' # Security Key by Yubico with NFC - Enterprise Edition (Firmware 5.4)
+
+                        # From https://github.com/passkeydeveloper/passkey-authenticator-aaguids/blob/main/aaguid.json
+                        # 'dd4ec289-e01d-41c9-bb89-70fa845d4bf2' # Apple iCloud Keychain (Managed)
+                        # 'd548826e-79b4-db40-a3d8-11116f7e8349' # Bitwarden
+                        # 'adce0002-35bc-c60a-648b-0b25f1f05503' # Chrome on Mac
+                        # 'b5397666-4885-aa6b-cebf-e52262a439a2' # Chromium Browser
+                        # '531126d6-e717-415c-9320-3d9aa6981239' # Dashlane
+                        # '771b48fd-d3d4-4f74-9232-fc157ab0507a' # Edge on Mac
+                        # 'f3809540-7f14-49c1-a8b3-8f813b225541' # Enpass
+                        # 'ea9b8d66-4d01-1d21-3ce4-b6b48cb575d4' # Google Password Manager
+                        # '39a5647e-1853-446c-a1f6-a79bae9f5bc7' # IDmelon
+                        # '0ea242b4-43c4-4a1b-8b17-dd6d0b6baec6' # Keeper
+                        # 'b84e4048-15dc-4dd0-8640-f4f60813c8af' # NordPass
+                        # '53414d53-554e-4700-0000-000000000000' # Samsung Pass
+                        # '08987058-cadc-4b81-b6e1-30de50dcbe96' # Windows Hello Hardware Authenticator
+                        # '6028b017-b1d4-4c02-b4b3-afcdafc96bb2' # Windows Hello Software Authenticator
+                        # '9ddd1817-af5a-4672-a2b9-3e3dd95000a9' # Windows Hello VBS Hardware Authenticator
+                        # 'bada5566-a7aa-401f-bd96-45619a55120d' # 1Password
                     )
                 }
             }
@@ -153,6 +207,24 @@ $EntraCAAuthStrengths = @(
                         '149a2021-8ef6-4133-96b8-81f8d5b7f1f5' # Security Key by Yubico with NFC (Firmware 5.2, 5.4)
                         'a4e9fc6d-4cbe-4758-b8ba-37598bb5bbaa' # Security Key by Yubico with NFC - Black (Firmware 5.4)
                         '0bb43545-fd2c-4185-87dd-feb0b2916ace' # Security Key by Yubico with NFC - Enterprise Edition (Firmware 5.4)
+
+                        # From https://github.com/passkeydeveloper/passkey-authenticator-aaguids/blob/main/aaguid.json
+                        # 'dd4ec289-e01d-41c9-bb89-70fa845d4bf2' # Apple iCloud Keychain (Managed)
+                        # 'd548826e-79b4-db40-a3d8-11116f7e8349' # Bitwarden
+                        # 'adce0002-35bc-c60a-648b-0b25f1f05503' # Chrome on Mac
+                        # 'b5397666-4885-aa6b-cebf-e52262a439a2' # Chromium Browser
+                        # '531126d6-e717-415c-9320-3d9aa6981239' # Dashlane
+                        # '771b48fd-d3d4-4f74-9232-fc157ab0507a' # Edge on Mac
+                        # 'f3809540-7f14-49c1-a8b3-8f813b225541' # Enpass
+                        # 'ea9b8d66-4d01-1d21-3ce4-b6b48cb575d4' # Google Password Manager
+                        # '39a5647e-1853-446c-a1f6-a79bae9f5bc7' # IDmelon
+                        # '0ea242b4-43c4-4a1b-8b17-dd6d0b6baec6' # Keeper
+                        # 'b84e4048-15dc-4dd0-8640-f4f60813c8af' # NordPass
+                        # '53414d53-554e-4700-0000-000000000000' # Samsung Pass
+                        # '08987058-cadc-4b81-b6e1-30de50dcbe96' # Windows Hello Hardware Authenticator
+                        # '6028b017-b1d4-4c02-b4b3-afcdafc96bb2' # Windows Hello Software Authenticator
+                        # '9ddd1817-af5a-4672-a2b9-3e3dd95000a9' # Windows Hello VBS Hardware Authenticator
+                        # 'bada5566-a7aa-401f-bd96-45619a55120d' # 1Password
                     )
                 }
             }
@@ -202,6 +274,24 @@ $EntraCAAuthStrengths = @(
                         '149a2021-8ef6-4133-96b8-81f8d5b7f1f5' # Security Key by Yubico with NFC (Firmware 5.2, 5.4)
                         'a4e9fc6d-4cbe-4758-b8ba-37598bb5bbaa' # Security Key by Yubico with NFC - Black (Firmware 5.4)
                         '0bb43545-fd2c-4185-87dd-feb0b2916ace' # Security Key by Yubico with NFC - Enterprise Edition (Firmware 5.4)
+
+                        # From https://github.com/passkeydeveloper/passkey-authenticator-aaguids/blob/main/aaguid.json
+                        'dd4ec289-e01d-41c9-bb89-70fa845d4bf2' # Apple iCloud Keychain (Managed)
+                        'd548826e-79b4-db40-a3d8-11116f7e8349' # Bitwarden
+                        'adce0002-35bc-c60a-648b-0b25f1f05503' # Chrome on Mac
+                        'b5397666-4885-aa6b-cebf-e52262a439a2' # Chromium Browser
+                        '531126d6-e717-415c-9320-3d9aa6981239' # Dashlane
+                        '771b48fd-d3d4-4f74-9232-fc157ab0507a' # Edge on Mac
+                        'f3809540-7f14-49c1-a8b3-8f813b225541' # Enpass
+                        'ea9b8d66-4d01-1d21-3ce4-b6b48cb575d4' # Google Password Manager
+                        '39a5647e-1853-446c-a1f6-a79bae9f5bc7' # IDmelon
+                        '0ea242b4-43c4-4a1b-8b17-dd6d0b6baec6' # Keeper
+                        'b84e4048-15dc-4dd0-8640-f4f60813c8af' # NordPass
+                        '53414d53-554e-4700-0000-000000000000' # Samsung Pass
+                        '08987058-cadc-4b81-b6e1-30de50dcbe96' # Windows Hello Hardware Authenticator
+                        # '6028b017-b1d4-4c02-b4b3-afcdafc96bb2' # Windows Hello Software Authenticator
+                        '9ddd1817-af5a-4672-a2b9-3e3dd95000a9' # Windows Hello VBS Hardware Authenticator
+                        'bada5566-a7aa-401f-bd96-45619a55120d' # 1Password
                     )
                 }
             }
@@ -240,6 +330,24 @@ $EntraCAAuthStrengths = @(
                         '149a2021-8ef6-4133-96b8-81f8d5b7f1f5' # Security Key by Yubico with NFC (Firmware 5.2, 5.4)
                         'a4e9fc6d-4cbe-4758-b8ba-37598bb5bbaa' # Security Key by Yubico with NFC - Black (Firmware 5.4)
                         '0bb43545-fd2c-4185-87dd-feb0b2916ace' # Security Key by Yubico with NFC - Enterprise Edition (Firmware 5.4)
+
+                        # From https://github.com/passkeydeveloper/passkey-authenticator-aaguids/blob/main/aaguid.json
+                        'dd4ec289-e01d-41c9-bb89-70fa845d4bf2' # Apple iCloud Keychain (Managed)
+                        'd548826e-79b4-db40-a3d8-11116f7e8349' # Bitwarden
+                        'adce0002-35bc-c60a-648b-0b25f1f05503' # Chrome on Mac
+                        'b5397666-4885-aa6b-cebf-e52262a439a2' # Chromium Browser
+                        '531126d6-e717-415c-9320-3d9aa6981239' # Dashlane
+                        '771b48fd-d3d4-4f74-9232-fc157ab0507a' # Edge on Mac
+                        'f3809540-7f14-49c1-a8b3-8f813b225541' # Enpass
+                        'ea9b8d66-4d01-1d21-3ce4-b6b48cb575d4' # Google Password Manager
+                        '39a5647e-1853-446c-a1f6-a79bae9f5bc7' # IDmelon
+                        '0ea242b4-43c4-4a1b-8b17-dd6d0b6baec6' # Keeper
+                        'b84e4048-15dc-4dd0-8640-f4f60813c8af' # NordPass
+                        '53414d53-554e-4700-0000-000000000000' # Samsung Pass
+                        '08987058-cadc-4b81-b6e1-30de50dcbe96' # Windows Hello Hardware Authenticator
+                        # '6028b017-b1d4-4c02-b4b3-afcdafc96bb2' # Windows Hello Software Authenticator
+                        '9ddd1817-af5a-4672-a2b9-3e3dd95000a9' # Windows Hello VBS Hardware Authenticator
+                        'bada5566-a7aa-401f-bd96-45619a55120d' # 1Password
                     )
                 }
             }
@@ -278,6 +386,24 @@ $EntraCAAuthStrengths = @(
                         '149a2021-8ef6-4133-96b8-81f8d5b7f1f5' # Security Key by Yubico with NFC (Firmware 5.2, 5.4)
                         'a4e9fc6d-4cbe-4758-b8ba-37598bb5bbaa' # Security Key by Yubico with NFC - Black (Firmware 5.4)
                         '0bb43545-fd2c-4185-87dd-feb0b2916ace' # Security Key by Yubico with NFC - Enterprise Edition (Firmware 5.4)
+
+                        # From https://github.com/passkeydeveloper/passkey-authenticator-aaguids/blob/main/aaguid.json
+                        'dd4ec289-e01d-41c9-bb89-70fa845d4bf2' # Apple iCloud Keychain (Managed)
+                        'd548826e-79b4-db40-a3d8-11116f7e8349' # Bitwarden
+                        'adce0002-35bc-c60a-648b-0b25f1f05503' # Chrome on Mac
+                        'b5397666-4885-aa6b-cebf-e52262a439a2' # Chromium Browser
+                        '531126d6-e717-415c-9320-3d9aa6981239' # Dashlane
+                        '771b48fd-d3d4-4f74-9232-fc157ab0507a' # Edge on Mac
+                        'f3809540-7f14-49c1-a8b3-8f813b225541' # Enpass
+                        'ea9b8d66-4d01-1d21-3ce4-b6b48cb575d4' # Google Password Manager
+                        '39a5647e-1853-446c-a1f6-a79bae9f5bc7' # IDmelon
+                        '0ea242b4-43c4-4a1b-8b17-dd6d0b6baec6' # Keeper
+                        'b84e4048-15dc-4dd0-8640-f4f60813c8af' # NordPass
+                        '53414d53-554e-4700-0000-000000000000' # Samsung Pass
+                        '08987058-cadc-4b81-b6e1-30de50dcbe96' # Windows Hello Hardware Authenticator
+                        # '6028b017-b1d4-4c02-b4b3-afcdafc96bb2' # Windows Hello Software Authenticator
+                        '9ddd1817-af5a-4672-a2b9-3e3dd95000a9' # Windows Hello VBS Hardware Authenticator
+                        'bada5566-a7aa-401f-bd96-45619a55120d' # 1Password
                     )
                 }
             }
@@ -314,6 +440,24 @@ $EntraCAAuthStrengths = @(
                         '149a2021-8ef6-4133-96b8-81f8d5b7f1f5' # Security Key by Yubico with NFC (Firmware 5.2, 5.4)
                         'a4e9fc6d-4cbe-4758-b8ba-37598bb5bbaa' # Security Key by Yubico with NFC - Black (Firmware 5.4)
                         '0bb43545-fd2c-4185-87dd-feb0b2916ace' # Security Key by Yubico with NFC - Enterprise Edition (Firmware 5.4)
+
+                        # From https://github.com/passkeydeveloper/passkey-authenticator-aaguids/blob/main/aaguid.json
+                        'dd4ec289-e01d-41c9-bb89-70fa845d4bf2' # Apple iCloud Keychain (Managed)
+                        'd548826e-79b4-db40-a3d8-11116f7e8349' # Bitwarden
+                        'adce0002-35bc-c60a-648b-0b25f1f05503' # Chrome on Mac
+                        'b5397666-4885-aa6b-cebf-e52262a439a2' # Chromium Browser
+                        '531126d6-e717-415c-9320-3d9aa6981239' # Dashlane
+                        '771b48fd-d3d4-4f74-9232-fc157ab0507a' # Edge on Mac
+                        'f3809540-7f14-49c1-a8b3-8f813b225541' # Enpass
+                        'ea9b8d66-4d01-1d21-3ce4-b6b48cb575d4' # Google Password Manager
+                        '39a5647e-1853-446c-a1f6-a79bae9f5bc7' # IDmelon
+                        '0ea242b4-43c4-4a1b-8b17-dd6d0b6baec6' # Keeper
+                        'b84e4048-15dc-4dd0-8640-f4f60813c8af' # NordPass
+                        '53414d53-554e-4700-0000-000000000000' # Samsung Pass
+                        '08987058-cadc-4b81-b6e1-30de50dcbe96' # Windows Hello Hardware Authenticator
+                        # '6028b017-b1d4-4c02-b4b3-afcdafc96bb2' # Windows Hello Software Authenticator
+                        '9ddd1817-af5a-4672-a2b9-3e3dd95000a9' # Windows Hello VBS Hardware Authenticator
+                        'bada5566-a7aa-401f-bd96-45619a55120d' # 1Password
                     )
                 }
             }
